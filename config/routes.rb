9dd7d946f1cf20527a84955699a5c931110a264a
Will Carron <wacarron27@gmail.com>
2015-05-04 18:32:52 -0700

    second commit. Added bootstrap reqs for the asset pipeline, resources for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   # This action should be created in the routes and controllers to create a splash page!
+   # ------------------------------------------------------------------------------------
+   root 'static_pages#splash'
+
+   resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
