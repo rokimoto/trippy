acd578374540285c894342cc380a414e30c749b4
Rachel Okimoto <rachel.okimoto@gmail.com>
2015-05-14 12:03:36 -0700

    fixed heroku background image bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # background image fix for heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
