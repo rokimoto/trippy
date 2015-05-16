548fdb65ce86deb89c7889733f08e984a20daef4
Will Carron <wacarron27@gmail.com>
2015-05-16 15:04:01 -0700

    finished all testing. Total of 27 tests: 8 controller, 19 model

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe LocationsController, type: :controller do
+RSpec.describe SessionsController, type: :controller do
 
    describe "GET #new" do
-     it "returns http success" do
-       get :new
-       expect(response).to have_http_status(:success)
-     end
-   end
-
-   describe "GET #index" do
       it 'returns http success' do
-         get :index
+         get :new
          expect(response).to have_http_status(:success)
       end
    end
 
    describe "POST #create" do
       it 'returns http success' do
-         get :index
          expect(response).to have_http_status(:success)
       end
+   end
+
+   describe "DELETE #destroy" do
+      it 'returns http success' do
+         get :new
+         expect(response).to have_http_status(:success)
+
+      end
+
    end
 
 end
